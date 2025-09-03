4522936bcc28f96441f7f0eff00f4ed871689d2e
rockey223 <rockeym50@gmail.com>
2025-09-03 10:37:02 +0545

    first commit

---
+++ src/app/lib/store.js
@@ -0,0 +1,37 @@
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import storage from "./createWebStorage";
+import { persistReducer, persistStore } from "redux-persist";
+import {
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+
+import themeReducer from "./actions/themeSlice";
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const rootReducer = combineReducers({
+  theme: themeReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export default store;
+export const persistor = persistStore(store);
